a2021b15232b3f598e580777e9b5ed3cd757076c
Suresh Hastimal Jat <sjat112200@gmail.com>
2024-07-11 17:01:22 +0530

    Clean and reset.

--- frontend/src/modules/home/components/hero/index.tsx
+++ frontend/src/modules/home/components/hero/index.tsx
@@ -10,15 +10,24 @@ const Hero = () => {
             level="h1"
             className="text-3xl leading-10 text-ui-fg-base font-normal"
           >
-            Saree Store
+            Ecommerce Starter Template
           </Heading>
           <Heading
             level="h2"
             className="text-3xl leading-10 text-ui-fg-subtle font-normal"
           >
-            Since 1998
+            Powered by Medusa and Next.js
           </Heading>
         </span>
+        <a
+          href="https://github.com/medusajs/nextjs-starter-medusa"
+          target="_blank"
+        >
+          <Button variant="secondary">
+            View on GitHub
+            <Github />
+          </Button>
+        </a>
       </div>
     </div>
   )
